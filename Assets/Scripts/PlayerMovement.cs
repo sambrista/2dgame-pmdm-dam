ddb0b22d146c8e1d0300d8e69c8d25ff74942fc7
Alfonso Jiménez <yo@alfonsojimenez.net>
2024-09-24 09:13:53 +0200

    Añadido salto básico
    Añadido salto básico, falta comprobar que no salte en el aire

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,15 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 	public Rigidbody2D rb;
+    
+    [Header("Movement")]
 	public float moveSpeed = 5f;
-	
 	float horizontalMovement;
+
+    [Header("Jumping")]
+    public float jumpPower = 10f;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -24,4 +30,12 @@ public class PlayerMovement : MonoBehaviour
 	public void Move(InputAction.CallbackContext context) {
 		horizontalMovement = context.ReadValue<Vector2>().x;
 	}
+
+    public void Jump(InputAction.CallbackContext context) {
+        if (context.performed) {
+            rb.velocity=new Vector2(rb.velocity.x, jumpPower);
+        } else if (context.canceled) {
+            rb.velocity=new Vector2(rb.velocity.x, jumpPower * 0.5f);
+        }
+    }
 }
