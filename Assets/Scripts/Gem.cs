b33bb0868c7ea7314064ba78d3f0644f8c39d145
Alfonso Jiménez <yo@alfonsojimenez.net>
2024-10-03 12:11:12 +0200

    Coleccionable

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour, IItem
+{
+    public void Collect() {
+        Destroy(gameObject);
+    }
+}
